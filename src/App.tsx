4b612f58962335eaf8844b0b8173b9eb16a1e5b7
ruanmendes <mendesjuan@gmail.com>
2025-02-25 16:27:46 -0300

    Make change to check deployment
    Added my name to the task manager's title

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="min-h-screen bg-gray-100 p-4">
       <header className="text-center mb-4">
-        <h1 className="text-3xl font-bold">Task Manager</h1>
+        <h1 className="text-3xl font-bold">Ruan's Task Manager</h1>
       </header>
 
       <TaskManager />
